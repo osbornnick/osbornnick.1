225031d91f9d99d2e9ec21267e9fed70b19e765c
osbornnick <nicholas.p.osborn@gmail.com>
2019-11-15 14:14:20 -0500

    post.md created

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,10 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    body = models.TextField()
+    md = models.FileField(null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+
+    def __str__(self):
+        return self.title
