1e9e3b674da09865a7e98b2503c7a5ff9fe8a4fa
osbornnick <nicholas.p.osborn@gmail.com>
2019-11-18 12:36:18 -0500

    removed Post.description

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,6 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField()
     md = models.FileField(null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
