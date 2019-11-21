29ea412f92824c27de965206f804a9bf1dd23c07
osbornnick <nicholas.p.osborn@gmail.com>
2019-11-21 10:48:41 -0500

    blog_category.hmtl

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.blog_index, name="blog_index"),
-    path("<str:slug>/", views.blog_post, name="blog_post"),
-    path("<category>/", views.blog_category, name="blog_category")
+    path("post/<slug:slug>/", views.blog_post, name="blog_post"),
+    path("category/<category>/", views.blog_category, name="blog_category")
 ]
