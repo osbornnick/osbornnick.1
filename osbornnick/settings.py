f225267ff44528e14b9e0bccb4c2b0afa965dd38
osbornnick <nicholas.p.osborn@gmail.com>
2019-11-21 16:15:48 -0500

    static files moved to subapp

--- osbornnick/settings.py
+++ osbornnick/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vj!h7ioc0$5uj_)+g%p)=y%9%v_o#l2co$g01#(w4+n_6nk$j_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['osbornnick.com', '142.93.119.57']
+# ALLOWED_HOSTS = ['osbornnick.com', '142.93.119.57']
 
 
 # Application definition
@@ -131,7 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
