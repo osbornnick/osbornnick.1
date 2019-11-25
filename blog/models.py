a86aebaedcebcd8b9e39736021d315585564a9df
osbornnick <nicholas.p.osborn@gmail.com>
2019-11-25 11:43:50 -0500

    save html as Post.content

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import markdown
 
 
 class Category(models.Model):
@@ -12,6 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     md = models.FileField(null=True)
+    content = models.TextField(default="default")
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
@@ -21,3 +23,13 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created_on']
+
+    def save(self, *args, **kwargs):
+        file = self.md
+        decoded = file.read().decode('utf-8')
+        extensions = ['fenced_code',
+                      'codehilite']
+        output = markdown.markdown(decoded, extensions=extensions)
+        self.content = output
+
+        super().save(*args, **kwargs)
