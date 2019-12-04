8756080fc250d01a07e7f53383508958d084e49b
osbornnick <nicholas.p.osborn@gmail.com>
2019-12-04 11:56:57 -0500

    .env to remove sensitive info

--- osbornnick/settings.py
+++ osbornnick/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vj!h7ioc0$5uj_)+g%p)=y%9%v_o#l2co$g01#(w4+n_6nk$j_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['osbornnick.com', '142.93.119.57', 'localhost']
 
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'osbornnick.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'osbornnickdb',
-        'USER': 'osbornnick',
-        'PASSWORD': 'usrpwd',
-        'HOST': 'localhost',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '',
     }
 }
