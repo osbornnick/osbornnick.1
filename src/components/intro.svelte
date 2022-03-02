a591c3cd13c7055197cb6ca002e70a2eea02d4d9
Nick Osborn <nicholas.p.osborn@gmail.com>
2022-03-02 15:42:32 -0500

    link styling more classic

--- src/components/intro.svelte
+++ src/components/intro.svelte
@@ -5,7 +5,11 @@
 <div class="flex flex-row justify-evenly md:w-2/3 mx-auto flex-wrap-reverse">
     <div class="font-serif m-5">
         <h1 class="text-2xl italic">Hi, I'm</h1>
-        <h1 class="text-4xl">Nick Osborn.</h1>
+        <h1
+            class="text-4xl bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent"
+        >
+            Nick Osborn.
+        </h1>
         <p>I like to write code and bake bread.</p>
         <p>I play piano, D&D, and climb rocks.</p>
         <p>
@@ -16,3 +20,6 @@
     </div>
     <img src={"/me-circle.png"} alt="me smiling" class="m-5 w-48" />
 </div>
+
+<style>
+</style>
--- src/components/link.svelte
+++ src/components/link.svelte
@@ -1,33 +1,28 @@
 <script>
     export let href;
-    // const colors = ["#FADA5E", "#5ECCFA", "#8C5EFA"];
-    // let color;
-    // $: color;
-    // import { onMount } from "svelte";
+    const colors = ["#fada5e", "#5eccfa", "#8c5efa"];
+    let color;
+    $: color;
+    import { onMount } from "svelte";
 
-    // onMount(() => {
-    //     color = colors[Math.floor(Math.random() * colors.length)];
-    // });
+    onMount(() => {
+        color = colors[Math.floor(Math.random() * colors.length)];
+    });
 </script>
 
-<a class="font-bold" {href}><slot /></a>
+<a
+    class="dark:text-slate-300 underline underline-offset-2 hover:text-blue-500 dark:hover:text-blue-500"
+    {href}><slot /></a
+>
 
 <style>
-    a {
-        background: linear-gradient(to right, #30cfd0, #c43ad6);
-        background-clip: text;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-    }
-
-    /* old styling */
     /* a {
         background-color: transparent;
         transition: box-shadow 200ms;
         transition-timing-function: cubic-bezier(0.18, 0.89, 0.32, 1.28);
-        box-shadow: 0 -3px 0 0 var(--color) inset;
+        box-shadow: 0 -3px 0 0 inset;
     }
     a:hover {
-        box-shadow: 0 -10px 0 0 var(--color) inset;
+        box-shadow: 0 -10px 0 0 inset;
     } */
 </style>
